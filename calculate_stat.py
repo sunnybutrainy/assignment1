f0646fde7b751bd55fe4bd8090cad76afc9ec57e
Eleandro G <130412327+E-debug-dotcom@users.noreply.github.com>
2024-09-20 15:23:33 -0700

    calculate_stat.py
    added variables and functions to compare the average run time for java, python and C

--- calculate_stat.py
+++ calculate_stat.py
@@ -13,4 +13,16 @@ with open('java_results.txt', 'r') as file:
     for line in file:
         java_results.append(int(line.strip()))
 
-# Can compare stats here like averages n stuff
+
+java_total = sum(java_results) 
+c_total = sum(c_results)
+py_total = sum(py_results)
+
+if numbers:  
+    average_java = java_total / len(java_results)
+    average_c = c_total / len(c_results)
+    average_py = py_total / len(py_results)
+    print(f"The average time for Java is:" +  {average_java} + " The average time for C is:" + {average_c} + "The average time for Python is: " + {average_py})
+
+else:
+    print("The list is empty.")
